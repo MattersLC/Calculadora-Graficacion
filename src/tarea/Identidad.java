1bddda36a94e026db90173f059e3255252259fc5
Orlando Lucero <orlando.lucero.camacho@gmail.com>
2022-11-05 17:28:55 -0600

    Creación de repositorio

---
+++ src/tarea/Identidad.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Tarea;
+
+/**
+ *
+ * @author "eliud"
+ */
+public class Identidad {
+    
+    public static void dibujar(Plano p){
+        for(float x=-10;x<10;x+=.1){
+            float y= x;
+            p.dibujarPunto(x, y);
+        }
+    }
+
+}
